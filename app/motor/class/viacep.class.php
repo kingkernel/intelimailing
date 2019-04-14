a518281359d9d2903731ba8603d1a9f8bf79fdaa
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-04-14 10:01:57 -0300

    up

--- app/motor/class/viacep.class.php
+++ app/motor/class/viacep.class.php
@@ -1,5 +1,13 @@
 <?php
 class viacep{
-	
+	public $cep;
+	public $format = "json";
+	public function __construct(){
+
+	}
+	public function getOnCep(){
+		$url = 'viacep.com.br/ws/'.$this->cep.'/'.$this->format.'/unicode';
+		$url = curl_init();
+	}
 }
-?>
+?
